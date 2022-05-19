3a6dfc4768e180c1a6936f0a10e02f74025386bb
stevecyj <ycsteven.tsao@gmail.com>
2022-05-19 16:06:45 +0800

    add auto-imports pkg

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig } from 'vite';
 import { viteMockServe } from 'vite-plugin-mock';
+import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
   base: '/nft-meta-wall-frontend/', // gh-pages 設定路徑
@@ -9,6 +10,10 @@ export default defineConfig({
     vue(),
     viteMockServe({
       supportTs: true,
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router'], // 自動引入的 api
+      dts: 'src/auto-imports.js',
     }),
   ],
   resolve: {
