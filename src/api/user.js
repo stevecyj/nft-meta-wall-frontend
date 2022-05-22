d8aecd7a1d4d9ce4b1026bf524c7e97606a26a46
root <root@localhost.localdomain>
2022-05-22 15:36:42 +0800

    feat. user login,access api

--- src/api/user.js
+++ src/api/user.js
@@ -1,16 +1,16 @@
-import request from '@/utils/request';
+import request from "@/utils/request";
 
 export function login(data) {
   return request({
-    url: '/login',
-    method: 'post',
-    data
-  })
+    url: "/users/sign_in",
+    method: "post",
+    data,
+  });
 }
 
 export function logout() {
   return request({
-    url: '/logout',
-    method: 'post'
-  })
+    url: "/logout",
+    method: "post",
+  });
 }
