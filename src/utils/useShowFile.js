4101c77341484869999448783c0e3d633716caa1
stevecyj <ycsteven.tsao@gmail.com>
2022-06-04 09:58:03 +0800

    fix. avatar no show when switch page

--- src/utils/useShowFile.js
+++ src/utils/useShowFile.js
@@ -1,3 +1,23 @@
+const errorMessageEnum = {
+  noContent: "沒有貼文內容，請填寫",
+  imageSizeError: "圖片檔案過大，僅限 1mb 以下檔案",
+  imageTypeError: "圖片格式錯誤，僅限 JPG、PNG 圖片",
+};
+let file = reactive({}); // 準備拿 input type="file" 的值
+let fs = reactive({
+  name: "", // input 的圖檔名稱
+  thumbnail: null, // input 的圖片縮圖
+  size: null, // input 的圖片大小
+  unit: "KB",
+});
+let title = ref(""); // 圖片標題
+let content = ref(""); // 貼文描述
+let preViewImage = ref(null); // 圖片預覽
+let errorImageMessageVised = ref(false);
+let errorImageMessage = ref("");
+let errorContentMessageVised = ref(false);
+let errorContentMessage = ref("");
+// 準備拿 input type="file" 的值
 export const showFile = (e) => {
   file = e.target.files[0]; // input type="file" 的值
   fs.name = file.name; // input 的圖檔名稱
