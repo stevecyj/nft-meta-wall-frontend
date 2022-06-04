e40754805cfde793b935b3cd826685b77d5b1798
stevecyj <ycsteven.tsao@gmail.com>
2022-06-04 15:37:12 +0800

    feat. [EditUserInfo]upload avatar,preview

--- src/components/EditUserInfo.vue
+++ src/components/EditUserInfo.vue
@@ -2,17 +2,34 @@
   <div class="card round-radius">
     <div class="center-container">
       <div class="avatar-upload">
+        <!-- <div class="avatar">
+        </div> -->
         <div class="avatar">
           <img
+            v-if="preViewImage"
+            class="avatar__img"
+            :src="preViewImage"
+            alt="avatar"
+          />
+          <img
+            v-else
             class="avatar__img"
             :src="userInfo.avatar"
             alt="avatar"
           />
         </div>
-        <button
+        <label
+          for="uploadAvatar"
           class="text-btn black"
           style="margin-top: 20px;"
-        >上傳大頭照</button>
+        >上傳大頭照</label>
+        <input
+          id="uploadAvatar"
+          style="visibility:hidden;"
+          type="file"
+          accept="image/png, image/gif, image/jpeg"
+          @change="showFile"
+        >
       </div>
       <div class="user-info">
         <p class="user-info__label">暱稱</p>
@@ -78,11 +95,13 @@
           </label>
         </div>
       </div>
+
+      <!-- 錯誤訊息 -->
       <p
         class="user-info__error-message text-align-center"
         style="margin-top: 32px;"
       >
-        1.圖片寬高比必需為 1:1，請重新輸入
+        1.沒有貼文內容，請填寫
       </p>
       <p class="user-info__error-message text-align-center">
         2. 解析度寬度至少 300像素以上，請重新輸入
@@ -102,6 +121,7 @@
 import { defineComponent } from 'vue';
 import { useStore } from 'vuex';
 import { alertSuccess, alertError } from '@/utils/swal';
+import { imageTypeRule } from '@/utils/validation';
 
 export default defineComponent({
   name: 'EditUserInfo',
@@ -112,7 +132,44 @@ export default defineComponent({
     const userName = ref('');
     const avatar = ref('');
     const gender = ref('');
-    // const show = ref(false);
+    const errorMessageEnum = {
+      noContent: '沒有圖片，請選擇圖片上傳',
+      imageSizeError: '解析度寬度至少 300像素以上，請重新輸入',
+    };
+    let file = reactive({}); // 準備拿 input type="file" 的值
+    let fs = reactive({
+      name: '', // input 的圖檔名稱
+      thumbnail: null, // input 的圖片縮圖
+      size: null, // input 的圖片大小
+      unit: 'KB',
+    });
+    let title = ref(''); // 圖片標題
+    // let content = ref(''); // 貼文描述
+    let preViewImage = ref(null); // 圖片預覽
+    let errorImageMessageVised = ref(false);
+    let errorImageMessage = ref('');
+    // let errorContentMessageVised = ref(false);
+    // let errorContentMessage = ref('');
+
+    const showFile = (e) => {
+      console.log(e);
+      file = e.target.files[0]; // input type="file" 的值
+      fs.name = file.name; // input 的圖檔名稱
+      fs.thumbnail = window.URL.createObjectURL(file); // input 的圖片縮圖
+      fs.size = ~~(file.size * 0.001); // input 的圖片大小
+      title.value = fs.name; // 預設 input 的圖檔名稱為圖片上傳時的圖片標題
+      preViewImage.value = URL.createObjectURL(file);
+
+      if (fs.size > 1000) {
+        errorImageMessageVised.value = true;
+        errorImageMessage.value = errorMessageEnum.imageSizeError;
+      } else if (!imageTypeRule(fs.name)) {
+        errorImageMessageVised.value = true;
+        errorImageMessage.value = errorMessageEnum.imageTypeError;
+      } else {
+        errorImageMessageVised.value = false;
+      }
+    };
 
     const userInfo = computed(() => {
       return store.getters['user/userInfo'];
@@ -149,11 +206,16 @@ export default defineComponent({
     };
 
     return {
+      errorImageMessageVised,
+      errorImageMessage,
+      preViewImage,
+      fs,
       userName,
       avatar,
       gender,
       userInfo,
       updateProfile,
+      showFile,
     };
   },
 });
