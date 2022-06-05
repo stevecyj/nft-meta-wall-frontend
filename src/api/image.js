7005af95ed1a57a11df3cb01153a2ff39daab599
stevecyj <ycsteven.tsao@gmail.com>
2022-06-05 14:40:44 +0800

    feat. upload avatar

--- src/api/image.js
+++ src/api/image.js
@@ -1,22 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
+import request from "@/utils/request";
 
-const imgurURL = 'https://api.imgur.com/3/image';
+const imgurURL = "https://api.imgur.com/3/image";
 
 export function uploadImage(file) {
   const formData = new FormData();
-  formData.append('image', file.file);
-  formData.append('title', file.title);
-  formData.append('description', file.content);
-  formData.append('album', import.meta.env.VITE_BASE_IMGUR_ALBUM);
+  formData.append("image", file.file);
+  formData.append("title", file.title);
+  formData.append("description", file.content);
+  formData.append("album", import.meta.env.VITE_BASE_IMGUR_ALBUM);
 
   let settings = {
     method: "post",
     url: imgurURL,
     headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_BASE_IMGUR_TOKEN}`
+      Authorization: `Bearer ${import.meta.env.VITE_BASE_IMGUR_TOKEN}`,
     },
     data: formData,
   };
 
   return axios(settings);
+}
+
+export function uploadAvatar(file) {
+  const formData = new FormData();
+  formData.append("image", file.file);
+  formData.append("title", file.title);
+  formData.append("description", file.content);
+  return request({
+    url: "/upload/image",
+    method: "post",
+    data: formData,
+  });
 }
