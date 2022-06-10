f3ab1524f95f628f443cbefb8facd4443e841b30
stevecyj <ycsteven.tsao@gmail.com>
2022-06-10 20:21:49 +0800

    feat. websocket,updatePost

--- src/views/PublishPost.vue
+++ src/views/PublishPost.vue
@@ -16,8 +16,12 @@
         </div>
       </div>
       <div style="margin-top: 16px;">
-        <label for="upload" class="text-btn black">上傳圖片</label>
-        <input id="upload"
+        <label
+          for="upload"
+          class="text-btn black"
+        >上傳圖片</label>
+        <input
+          id="upload"
           style="visibility:hidden;"
           type="file"
           accept="image/png, image/gif, image/jpeg"
@@ -30,13 +34,22 @@
         style="margin-top: 16px;"
         :src="preViewImage"
       />
-      <div v-else class="draft-post__img" />
+      <div
+        v-else
+        class="draft-post__img"
+      />
 
       <!-- 錯誤訊息 -->
-      <p v-if="errorImageMessageVised" class="error-message">
+      <p
+        v-if="errorImageMessageVised"
+        class="error-message"
+      >
         {{ errorImageMessage }}
       </p>
-      <p v-if="errorContentMessageVised" class="error-message">
+      <p
+        v-if="errorContentMessageVised"
+        class="error-message"
+      >
         {{ errorContentMessage }}
       </p>
       <div style="max-width: 323px; margin: 0 auto;">
@@ -53,20 +66,55 @@
 </template>
 
 <script>
-import { defineComponent, reactive, ref, watch, onMounted} from 'vue';
+import { defineComponent, reactive, ref, watch, onMounted, onUnmounted } from 'vue';
 import { useStore } from 'vuex';
 import { uploadImage } from '@/api/image.js';
 import { createPost } from '@/api/post';
 import router from '../router';
 import { imageTypeRule } from '@/utils/validation';
+import { io } from 'socket.io-client';
+import { getLocalStorageToken } from '@/utils/auth';
 
 export default defineComponent({
   name: 'PublishPost',
-  components: {
-  },
+  components: {},
   setup() {
     const store = useStore();
-    const errorMessageEnum  = {
+
+    // get follower id list
+    const followerIdList = ref([]);
+    const storeFollowerList = computed(() => {
+      const data = store.getters['user/followerList'];
+      data.forEach((item) => {
+        console.log(item.id._id);
+        followerIdList.value.push(item.id._id);
+      });
+      return data;
+    });
+
+    // socket
+    const host = import.meta.env.VITE_BASE_API;
+    const token = getLocalStorageToken();
+    const socket = io(host, {
+      path: '/socket.io/connect',
+      transports: ['websocket'],
+      autoConnect: true, //启动自从自动连接
+      secure: true,
+      transports: ['websocket'], // ['websocket', 'polling']
+      reconnection: true, //启动重新连接
+      reconnectionAttempts: 5, //最大重试连接次数
+      reconnectionDelay: 2000, //最初尝试新的重新连接等待时间
+      reconnectionDelayMax: 10000,
+      query: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    // 送貼文
+    const sendUpdateNotification = async () => {
+      socket.emit('updatePost');
+    };
+
+    const errorMessageEnum = {
       noContent: '沒有貼文內容，請填寫',
       imageSizeError: '圖片檔案過大，僅限 1mb 以下檔案',
       imageTypeError: '圖片格式錯誤，僅限 JPG、PNG 圖片',
@@ -76,7 +124,7 @@ export default defineComponent({
       name: '', // input 的圖檔名稱
       thumbnail: null, // input 的圖片縮圖
       size: null, // input 的圖片大小
-      unit: 'KB'
+      unit: 'KB',
     });
     let title = ref(''); // 圖片標題
     let content = ref(''); // 貼文描述
@@ -97,7 +145,7 @@ export default defineComponent({
       if (fs.size > 1000) {
         errorImageMessageVised.value = true;
         errorImageMessage.value = errorMessageEnum.imageSizeError;
-      } else if(!imageTypeRule(fs.name)){
+      } else if (!imageTypeRule(fs.name)) {
         errorImageMessageVised.value = true;
         errorImageMessage.value = errorMessageEnum.imageTypeError;
       } else {
@@ -111,7 +159,7 @@ export default defineComponent({
           file,
           title: title.value,
           description: content.value,
-        }
+        };
 
         const uploadResult = await uploadImage(form);
         console.log(uploadResult);
@@ -141,8 +189,15 @@ export default defineComponent({
         file?.size && (image = await submitImage());
 
         const userId = store.getters['user/userInfo']?.id; // login後要有值，目前使用預設
-        const { status, data } = await store.dispatch('post/addPost', { userId, tags: 'test', type: 'person',content: content.value, image });
+        const { status, data } = await store.dispatch('post/addPost', {
+          userId,
+          tags: 'test',
+          type: 'person',
+          content: content.value,
+          image,
+        });
         // 成功轉到動態牆
+        status === 'success' && sendUpdateNotification();
         status === 'success' && router.push({ path: '/home' });
       } catch (error) {
         console.log('create post error', error);
@@ -153,7 +208,7 @@ export default defineComponent({
     };
 
     // 檢查 content 值
-    watch(content, (val)=>{
+    watch(content, (val) => {
       if (val === '') {
         errorContentMessageVised.value = true;
         errorContentMessage.value = errorMessageEnum.noContent;
@@ -161,12 +216,15 @@ export default defineComponent({
         errorContentMessageVised.value = false;
       }
       // console.log(content.value);
-    })
+    });
     // 一開始 show content error
-    onMounted(()=>{
+    onMounted(() => {
       errorContentMessageVised.value = true;
       errorContentMessage.value = errorMessageEnum.noContent;
-    })
+    });
+    onUnmounted(() => {
+      socket.close();
+    });
 
     return {
       errorImageMessageVised,
@@ -175,11 +233,13 @@ export default defineComponent({
       errorContentMessage,
       content,
       preViewImage,
+      followerIdList,
+      storeFollowerList,
       showFile,
       submitPost,
       submitImage,
     };
-  }
+  },
 });
 </script>
 
